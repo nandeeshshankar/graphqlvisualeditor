4c856e2926de459fa0551d9153101249ab356f6f
Nandeesh Shankarappa <82076814+nandeeshshankar@users.noreply.github.com>
2022-11-08 23:18:15 +0530

    Create new graph and load existing feature
    Create new graph and load existing feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { GraphQLEditorComponent } from './GraphQLEditor';
+import { GraphQLEditor } from 'graphql-editor';
 
+const schemas = {
+  pizza: `
+    type Query{
+      pizzas: [Pizza!]
+    }
+    `,
+  pizzaLibrary: `
+    type Pizza{
+      name:String
+    }
+    `,
+  };
 
 function App() {
+  const inputRef = useRef(null);
+  
+  const [mySchema, setMySchema] = useState({
+    code: schemas.pizza,
+    libraries: schemas.pizzaLibrary
+  });
+  const placeholderData = "Start writing the schema";
+  let fileReader;
+
+  function handleImportFileClick(event){
+    inputRef.current.click();
+  }
+
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+    setMySchema({
+      code: content,
+      libraries: "",
+    });
+  };
+
+  const handleFileChosen = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+
+  const newGraph = () => {
+    setMySchema({
+      code: '',
+      libraries: ''
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,14 +61,49 @@ function App() {
           <Row>
             <Col md={1}>
               <br/>
-              <Button variant="primary">Create New Graph</Button><br/><br/>
-              <Button variant="primary">Import Existing</Button><br/><br/>
+              <input
+                style={{display: 'none'}}
+                ref={inputRef}
+                type="file"
+                onChange={e => handleFileChosen(e.target.files[0])}
+              />
+              <Button variant="primary" onClick={() => newGraph()}>Create New Graph</Button><br/><br/>
+              <Button variant="primary" onClick={handleImportFileClick}>Import Existing</Button><br/><br/>
               <Button variant="primary">Load Template</Button>
             </Col>
-            <Col><GraphQLEditorComponent /></Col>
+            <Col>
+    <div
+      style={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignSelf: 'stretch',
+        display: 'flex',
+        position: 'relative',
+      }}
+    >
+      <GraphQLEditor placeholder={placeholderData}
+        schema={mySchema}
+        setSchema={(props)=> {
+            console.log("setSchema called", props);
+            setMySchema(props);
+        }}
+        readonly={false}
+        //diffSchemas={oldSchema: mySchema; newSchema: mySchema}
+        activePane="diagram"
+        //state={pane: ActivePane}
+        onStateChange={()=>{
+          console.log("onStateChange called")
+        }}
+        
+        onTreeChange={(tree)=> {
+            console.log("onTreeChange called")
+        }}
+      />
+    </div>
+            </Col>
           </Row>  
         </Container>
-        {/* <GraphQLEditorComponent /> */}
       </header>
     </div>
   );
